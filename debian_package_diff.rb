82b2110850fe5bb8212bf9328d32f591f0dd3591
Fabio Perrella <fabio.perrella@gmail.com>
2013-01-23 10:52:44 -0200

    baixando arquivos para o diretorio tmp

--- debian_package_diff.rb
+++ debian_package_diff.rb
@@ -1,4 +1,8 @@
 #!/usr/bin/env ruby
+require 'tmpdir'
+require 'fileutils'
+TEMPDIR = "tmp"
+include FileUtils
 
 def main
   package_name = ARGV[0]
@@ -6,24 +10,33 @@ def main
   version2 = ARGV[2]
 
   puts "comparando pacote #{package_name} #{version1} e #{version2}"
-  # download_packages(package_name, version1, version2)
-
-  puts "==> comparando se existem arquivos a mais em algum pacote..."
-  system "debdiff #{package_name}_#{version1}_amd64.deb #{package_name}_#{version2}_amd64.deb"
-
-  puts "==> comparando se existem arquivos com conteudo diferente em cada pacote..."
-  system "debdiff #{package_name}_#{version1}.dsc #{package_name}_#{version2}.dsc"
+  download_packages(package_name, version1, version2)
+  compare_packages(package_name, version1, version2)
+  remove_packages
 
   puts "fim"
 end
 
+def compare_packages(package_name, version1, version2)
+  puts "==> comparando se existem arquivos a mais em algum pacote..."
+  system "debdiff #{TEMPDIR}/#{package_name}_#{version1}_amd64.deb #{TEMPDIR}/#{package_name}_#{version2}_amd64.deb"
+
+  puts "==> comparando se existem arquivos com conteudo diferente em cada pacote..."
+  system "debdiff #{TEMPDIR}/#{package_name}_#{version1}.dsc #{TEMPDIR}/#{package_name}_#{version2}.dsc"
+end
+
 def download_packages(package_name, version1, version2)
+  mkdir_p "tmp"
   base_url = "http://pacotes.linux.locaweb.com.br/ft_saas_squeeze/debian/pool/main/#{package_name[0]}/#{package_name}"
   [version1, version2].each do |version|
     [".dsc", ".tar.gz", "_amd64.deb"].each do |ext|
-      `wget #{base_url}/#{package_name}_#{version}#{ext}`
+      `wget -P tmp #{base_url}/#{package_name}_#{version}#{ext}`
     end
   end
+end
+
+def remove_packages
+  rm_rf "tmp"
 end
 
 main()
