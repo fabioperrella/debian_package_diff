2ae8c0aded603b9ec1da651fcb9189a457100d9d
Fabio Perrella <fabio.perrella@gmail.com>
2013-01-23 10:28:49 -0200

    roda debdiff para cada pacote

--- debian_package_diff.rb
+++ debian_package_diff.rb
@@ -1,5 +1,22 @@
 #!/usr/bin/env ruby
 
+def main
+  package_name = ARGV[0]
+  version1 = ARGV[1]
+  version2 = ARGV[2]
+
+  puts "comparando pacote #{package_name} #{version1} e #{version2}"
+  # download_packages(package_name, version1, version2)
+
+  puts "==> comparando se existem arquivos a mais em algum pacote..."
+  system "debdiff #{package_name}_#{version1}_amd64.deb #{package_name}_#{version2}_amd64.deb"
+
+  puts "==> comparando se existem arquivos com conteudo diferente em cada pacote..."
+  system "debdiff #{package_name}_#{version1}.dsc #{package_name}_#{version2}.dsc"
+
+  puts "fim"
+end
+
 def download_packages(package_name, version1, version2)
   base_url = "http://pacotes.linux.locaweb.com.br/ft_saas_squeeze/debian/pool/main/#{package_name[0]}/#{package_name}"
   [version1, version2].each do |version|
@@ -9,11 +26,4 @@ def download_packages(package_name, version1, version2)
   end
 end
 
-package_name = ARGV[0]
-version1 = ARGV[1]
-version2 = ARGV[2]
-
-puts "comparando pacote #{package_name} #{version1} e #{version2}"
-download_packages(package_name, version1, version2)
-
-puts "fim"
+main()
