7d0c7724b3abd02d1e338e4951a86733b2e7bf02
Fabio Perrella <fabio.perrella@gmail.com>
2013-01-23 11:01:33 -0200

    transforming to a class

--- debian_package_diff.rb
+++ debian_package_diff.rb
@@ -2,41 +2,48 @@
 require 'tmpdir'
 require 'fileutils'
 TEMPDIR = "tmp"
+REPOSITORY_URL = "http://pacotes.linux.locaweb.com.br/ft_saas_squeeze/debian/pool/main"
 include FileUtils
 
-def main
-  package_name = ARGV[0]
-  version1 = ARGV[1]
-  version2 = ARGV[2]
+class DebianPackageDiff
 
-  puts "comparando pacote #{package_name} #{version1} e #{version2}"
-  remove_packages
-  download_packages(package_name, version1, version2)
-  compare_packages(package_name, version1, version2)
+  def initialize(package_name, version1, version2)
+    @package_name = package_name
+    @version1 = version1
+    @version2 = version2
+  end
 
-  puts "fim"
-end
+  def compare
+    puts "comparando pacote #{@package_name} #{@version1} e #{@version2}"
+    remove_packages
+    download_packages
+    compare_packages
+    puts "fim"
+  end
 
-def compare_packages(package_name, version1, version2)
-  puts "==> comparando se existem arquivos a mais em algum pacote..."
-  system "debdiff #{TEMPDIR}/#{package_name}_#{version1}_amd64.deb #{TEMPDIR}/#{package_name}_#{version2}_amd64.deb"
+  private
 
-  puts "==> comparando se existem arquivos com conteudo diferente em cada pacote..."
-  system "debdiff #{TEMPDIR}/#{package_name}_#{version1}.dsc #{TEMPDIR}/#{package_name}_#{version2}.dsc"
-end
+  def compare_packages
+    puts "==> comparando se existem arquivos a mais em algum pacote..."
+    system "debdiff #{TEMPDIR}/#{@package_name}_#{@version1}_amd64.deb #{TEMPDIR}/#{@package_name}_#{@version2}_amd64.deb"
 
-def download_packages(package_name, version1, version2)
-  mkdir_p "tmp"
-  base_url = "http://pacotes.linux.locaweb.com.br/ft_saas_squeeze/debian/pool/main/#{package_name[0]}/#{package_name}"
-  [version1, version2].each do |version|
-    [".dsc", ".tar.gz", "_amd64.deb"].each do |ext|
-      `wget -P tmp #{base_url}/#{package_name}_#{version}#{ext}`
+    puts "==> comparando se existem arquivos com conteudo diferente em cada pacote..."
+    system "debdiff #{TEMPDIR}/#{@package_name}_#{@version1}.dsc #{TEMPDIR}/#{@package_name}_#{@version2}.dsc"
+  end
+
+  def download_packages
+    mkdir_p TEMPDIR
+    base_url = "#{REPOSITORY_URL}/#{@package_name[0]}/#{@package_name}"
+    [@version1, @version2].each do |version|
+      [".dsc", ".tar.gz", "_amd64.deb"].each do |ext|
+        `wget -P #{TEMPDIR} #{base_url}/#{@package_name}_#{version}#{ext}`
+      end
     end
+  end
+
+  def remove_packages
+    rm_rf TEMPDIR
   end
 end
 
-def remove_packages
-  rm_rf "tmp"
-end
-
-main()
+DebianPackageDiff.new(*ARGV).compare
